12fe8f18d9fb6f2f0b7cfc61a1405dcd06a72c2e
savalance98 <savindaniil29@gmail.com>
2021-12-17 18:05:57 +0300

    master

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 import pygame
 
-WIDTH, HEIGHT = 1024, 883
+WIDTH, HEIGHT = 660, 883
 
 fps = 60
 
@@ -18,10 +18,15 @@ paddle2_rect = int(paddle2_radius * 2 ** 0.5)
 paddle2 = pygame.Rect(330, 10, paddle2_rect, paddle2_rect)
 
 ball_radius = 20
-ball_speed = 6
+ball_speed = 2
 ball_rect = int(ball_radius * 2 ** 0.5)
 ball = pygame.Rect(WIDTH // 2, HEIGHT // 2, ball_rect, ball_rect)
 dx, dy = 1, - 1
+
+def pole():
+    pygame.draw.lines(sc, 'WHITE', True,
+                      [[10, 10], [140, 70],
+                       [280, 20]], 2)
 
 
 
@@ -30,18 +35,22 @@ pygame.init()
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-img = pygame.image.load('depositphotos_151759188-stock-illustration-north-american-ice-hockey-rink.jpg').convert()
+img = pygame.image.load('image_2021-12-17_17-50-53.png').convert()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
 
-    sc.blit(img, (0,0))
+    # sc.blit(img, (0,0))
+    sc.fill('black')
+    pole()
+
     pygame.draw.circle(sc, pygame.Color('red'),  paddle.center, paddle_radius)
     pygame.draw.circle(sc, pygame.Color('white'), ball.center, ball_radius)
     pygame.draw.circle(sc, pygame.Color('blue'), paddle2.center,paddle2_radius)
     ball.x += ball_speed * dx
     ball.y += ball_speed * dy
+    pygame.display.flip()
 
     if ball.centerx < ball_radius or ball.centerx > WIDTH - ball_radius:
         dx = -dx
@@ -49,6 +58,7 @@ while True:
         dy = -dy
     if ball.colliderect(paddle) and dy > 0:
         dy = -dy
+        print(paddle.x, paddle.y,ball.x,ball.y)
     if ball.colliderect(paddle2) and dy < 0:
         dy = -dy
 
